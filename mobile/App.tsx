529d33d23a11f0c1de2e29fa602f6f92d4a544a2
Jamersonze <jamersonze54@gmail.com>
2022-05-06 18:53:33 -0300

    Stage 4 - Mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar'
+import { View } from 'react-native'
+import Widget from './src/components/Widget'
+import { theme } from './src/theme'
+import {AppLoading} from 'expo-app-loading'
+import {useFonts, Inter_900Black} from '@expo-google-fonts/inter'
+
+
+export default function App() {
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+      <Widget />
+
+      <StatusBar
+        style={'light'}
+        backgroundColor={"transparent"}
+        translucent
+      />
+    </View>
+  )
+}
